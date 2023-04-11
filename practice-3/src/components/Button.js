359827761b49a164a32e8ae2830378e59cc32c78
Nhoj <nhojplummer@yahoo.com>
2023-04-11 11:34:15 -0400

    Finally bilt the component for props now I must make it reusable

--- practice-3/src/components/Button.js
+++ practice-3/src/components/Button.js
@@ -1,12 +1,11 @@
 import React from "react"
 
 function Button(props) {
-  const label = props.label
   const number = props.number
 
   return (
     <div>
-      <button onClick={() => alert(`You clicked on Button` + { number })}>Button {number}</button>
+      <button onClick={() => alert("You clicked on Button " + number)}>Button {number}</button>
     </div>
   )
 }
