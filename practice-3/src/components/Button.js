6140a6195667075987e4915ce62260e2b3320e61
Nhoj <nhojplummer@yahoo.com>
2023-04-11 10:18:51 -0400

    Created button component

---
+++ practice-3/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Button = number => {
+  return (
+    <div>
+      <button onClick={() => alert("Clicked!")}>Click Me</button>
+    </div>
+  )
+}
+
+export default Button
